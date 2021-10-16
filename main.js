44974de66ac8d6d16b61a4e998d02f1ee1fa72cb
nos64 <manosov1984@gmail.com>
2021-10-17 01:48:08 +0400

    Homework-3

--- main.js
+++ main.js
@@ -1,60 +1,105 @@
-
-//Task #0
+const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+const $reloadPage = document.querySelector('.button-reload')
 
 const player001 = {
+    player: 1,
     name: 'Boris the Blade',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['Katana', 'Gun', 'Bomb'],
     attack: function() {
-        console.log(player1.name + ' ' + 'Fight...');
+        console.log(player001.name + ' ' + 'Fight...');
     }
 };
 
 const player002 = {
+    player: 2,
     name: 'Brick Top',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Pig', 'Knife', 'Rifle'],
     attack: function() {
-        console.log(player2.name + ' ' + 'Fight...');
+        console.log(player002.name + ' ' + 'Fight...');
     }
 };
 
-// Task #1
-const $arenas = document.querySelector('.arenas');
-function createPlayer(player1, player001) {
-  
-
-  const $player1 = document.createElement('div');
-  $player1.classList.add(player1);
-  $arenas.appendChild($player1);
-
-  const $progressbar1 = document.createElement('div');
-  $progressbar1.classList.add('progressbar')
-  $player1.appendChild($progressbar1);
-
-    const $life1 = document.createElement('div');
-    $life1.classList.add('life');
-    $life1.style.width = player001.hp + '%';
-    $progressbar1.appendChild($life1);
-
-    const $name1 = document.createElement('div');
-    $name1.classList.add('name');
-    $name1.innerText = player001.name;
-    $progressbar1.appendChild($name1);
-
-  const $character1 = document.createElement('div');
-  $character1.classList.add('character');
-  $player1.appendChild($character1);
-     
-    const $img1 = document.createElement('img');
-    $img1.src = player001.img;
-    $character1.appendChild($img1)
+function createElement (tag, className) {
+    const $tag = document.createElement(tag);
+    if (className) {
+        $tag.classList.add(className);
+    }
+    return $tag;
 }
 
-//Task #2
+function createPlayer(playerObj) {
+    const $player = createElement('div', 'player' + playerObj.player);
+    const $progressbar = createElement('div', 'progressbar');
+    const $character = createElement('div','character');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $img = createElement('img');
 
-createPlayer('player1', player001);
-createPlayer('player2', player002);
+    $life.style.width = playerObj.hp + '%';
+    $name.innerText = playerObj.name;
+    $img.src = playerObj.img;
+    
+    $progressbar.appendChild($name);
+    $progressbar.appendChild($life);
+    $character.appendChild($img);
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+    
+    return $player;
+}
 
+$arenas.appendChild(createPlayer(player001));
+$arenas.appendChild(createPlayer(player002));
+
+function changeHP(player) {
+    const $playerLife = document.querySelector('.player' + player.player +' .life');
+    player.hp -= Math.ceil(Math.random() * 20);
+    $playerLife.style.width = player.hp + '%';
+    console.log(player.hp)
+    if (player.hp <= 0) {
+        player.hp = 0;
+        $playerLife.style.width = 0 + '%';
+        console.log(player.hp)
+        // $arenas.appendChild(playerLose(player.name));
+        $randomButton.disabled = true;
+        $randomButton.style.backgroundColor = "red";
+    }
+    showWinner()
+}
+
+// function playerLose(name) {
+//     const $loseTitle = createElement('div', 'loseTitle');
+//     $loseTitle.innerText = name + ' lose';
+
+//     return $loseTitle;
+// }
+
+function playerWin(name) {
+    const $winTitle = createElement('div', 'loseTitle');
+    $winTitle.innerText = name + ' win!';
+
+    return $winTitle;
+}
+
+$randomButton.addEventListener('click', function() {
+    changeHP(player001)
+    changeHP(player002)
+});
+
+
+function showWinner() {
+    if (player001.hp <= 0) {
+        $arenas.appendChild(playerWin(player002.name));
+    } else if (player002.hp <= 0) {
+        $arenas.appendChild(playerWin(player001.name));
+    }
+}
+
+$reloadPage.addEventListener('click', function() {
+    window.location.reload()
+});
